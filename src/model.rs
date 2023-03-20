47ba46bc6dec29881a26f190807b6ea4c84bd374
LinggaAskaEdo <lemp.otis@gmail.com>
2023-03-20 11:15:29 +0700

    finalize

---
+++ src/model.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize, sqlx::FromRow)]
+#[allow(non_snake_case)]
+pub struct NoteModel {
+    pub id: String,
+    pub title: String,
+    pub content: String,
+    pub category: Option<String>,
+    pub published: i8,
+    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
---
+++ src/response.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[allow(non_snake_case)]
+pub struct NoteModelResponse {
+    pub id: String,
+    pub title: String,
+    pub content: String,
+    pub category: String,
+    pub published: bool,
+    pub createdAt: chrono::DateTime<chrono::Utc>,
+    pub updatedAt: chrono::DateTime<chrono::Utc>,
+}
